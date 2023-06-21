d1eac086b673fa65cc45f320551c1ad7f5e06af3
zCris101 <chrstnmusico@gmail.com>
2023-06-21 21:53:56 +0800

    Done Basic Contacts Proj

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import "./styles.css";
+import Card from './components/Card';
+import Contacts from './components/contacts';
+
 //commented 
 export default function App() {
+
+  //console.log(Contacts);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1 className='heading'>My Contacts</h1>
+      <Card
+      name= {Contacts[0].name}
+      pic = {Contacts[0].imgURL}
+      phone = {Contacts[0].phone}
+      email = {Contacts[0].email} 
+      />
+
+      <Card
+      name= {Contacts[1].name}
+      pic = {Contacts[1].imgURL}
+      phone = {Contacts[1].phone}
+      email = {Contacts[1].email} 
+      />
+
+      <Card
+      name= {Contacts[2].name}
+      pic = {Contacts[2].imgURL}
+      phone = {Contacts[2].phone}
+      email = {Contacts[2].email} 
+      />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div>
+        
+        <div className='card'>
+            <div className='top'>
+                <h2 className='name'>{props.name}</h2>
+                <img 
+                className='circle-img'
+                src={props.pic} alt="avatar.png" />
+            </div>
+            <div className='bottom'>
+                <p className='info'>{props.phone}</p>
+                <p className='info'>{props.email}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Card
